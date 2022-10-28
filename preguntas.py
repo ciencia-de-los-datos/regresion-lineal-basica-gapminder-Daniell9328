a1916e99fbda9f8069be23116d672db6f93e255d
Daniell9328 <112450832+Daniell9328@users.noreply.github.com>
2022-10-28 13:15:33 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -147,7 +147,7 @@ def pregunta_04():
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
-    def pregunta_05():
+def pregunta_05():
     """
     Evaluación del modelo
     -------------------------------------------------------------------------------------
